cd16d694dfa1ba416f3fd4fb6d0162cdf801c576
sanjay kumar mahato <sanjay kumar mahato>
2020-02-28 08:57:11 +0530

    Sticky Row and Column

--- ExcelSheet/src/excuteFormulas/formulaHelper.ts
+++ ExcelSheet/src/excuteFormulas/formulaHelper.ts
@@ -1,6 +1,5 @@
 export  class FormulaHelper{
      static getLetter(number:number):string{
-        debugger
         if(number ===0){return;}
         let rem = number%26;
         let letter = String.fromCharCode(64 + rem);
